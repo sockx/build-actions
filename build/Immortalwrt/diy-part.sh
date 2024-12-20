2e82f292d4de8976300c33b2085cb52ae16ada81
sockx <32082618+sockx@users.noreply.github.com>
2024-12-20 11:32:37 +0800

    添加插件

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -4,6 +4,13 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
+# 不知道有没有 先删了
+rm -rf ./feeds/luci/applications/luci-app-passwall
+
+# 添加插件
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 
 # 后台IP设置
 export Ipv4_ipaddr="10.10.10.2"            # 修改openwrt后台地址(填0为关闭)
